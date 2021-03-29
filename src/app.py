38116d005397f31077f049dcbc68f173a2d10aff
Alexandre Dewilde <alexandredewilde2001@gmail.com>
2021-03-29 21:46:26 +0200

    add an example

--- src/app.py
+++ src/app.py
@@ -1,3 +1,5 @@
+import sqlite3
+
 from flask import Flask
 from flask import render_template
 
@@ -7,10 +9,21 @@ from config import DEBUG
 app = Flask(__name__)
 
 
+db = sqlite3.connect("./db/database.db", check_same_thread=False)
+cursor = db.cursor()
+
 @app.route("/")
 @app.route("/index")
 def index():
     return render_template("index.html")
+
+
+@app.route("/example")
+def exemple():
+    req = "SELECT * FROM animaux WHERE id == 1225"
+    animal = cursor.execute(req).fetchone()
+    animal_sexe = animal[2]
+    return f"Le sexe de l'animal est {animal_sexe}"
 
 
 if __name__ == "__main__":
