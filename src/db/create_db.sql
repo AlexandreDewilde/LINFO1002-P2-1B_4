7df8de25e06162ce247fdfccd953401c1d161d2d
Alexandre Dewilde <alexandredewilde2001@gmail.com>
2021-03-31 14:15:08 +0200

    correct indent of sql file

--- src/db/create_db.sql
+++ src/db/create_db.sql
@@ -6,11 +6,11 @@ CREATE TABLE animaux(
     apprivoise     INT  NOT NULL,                 
     mort_ne        INT  NOT NULL, 
     decede         INT  NOT NULL,
-    FOREIGN KEY(famille_id) REFERENCES familles(id)
+    FOREIGN KEY (famille_id) REFERENCES familles(id)
 );
 
 CREATE TABLE familles(
-    id INT PRIMARY KEY,
+    id  INT PRIMARY KEY,
     nom TEXT NOT NULL        
 );
 
@@ -20,40 +20,40 @@ CREATE TABLE types(
 );
 
 CREATE TABLE animaux_types(
-    animal_id INT NOT NULL,
-    type_id INT NOT NULL,
+    animal_id   INT NOT NULL,
+    type_id     INT NOT NULL,
     pourcentage REAL NOT NULL,
-    FOREIGN KEY(animal_id) REFERENCES animaux(id),
-    FOREIGN KEY(type_id) REFERENCES types(id),
-    PRIMARY KEY(animal_id, type_id)
+    FOREIGN KEY (animal_id) REFERENCES animaux(id),
+    FOREIGN KEY (type_id) REFERENCES types(id),
+    PRIMARY KEY (animal_id, type_id)
 );
 
 CREATE TABLE velages(
-    id INT PRIMARY KEY,
+    id      INT PRIMARY KEY,
     mere_id INT NOT NULL,
     pere_id INT NOT NULL,
-    date INT NOT NULL,
-    FOREIGN KEY(pere_id) REFERENCES animaux(id)
-    FOREIGN KEY(mere_id) REFERENCES animaux(id)
+    date    INT NOT NULL,
+    FOREIGN KEY (pere_id) REFERENCES animaux(id)
+    FOREIGN KEY (mere_id) REFERENCES animaux(id)
 );
 
 CREATE TABLE animaux_velages(
     animal_id INT NOT NULL,
     velage_id NOT NULL,
-    FOREIGN KEY(animal_id) REFERENCES animaux(id),
-    FOREIGN KEY(velage_id) REFERENCES velages(id),
-    PRIMARY KEY(animal_id, velage_id)
+    FOREIGN KEY (animal_id) REFERENCES animaux(id),
+    FOREIGN KEY (velage_id) REFERENCES velages(id),
+    PRIMARY KEY (animal_id, velage_id)
 );
 
 CREATE TABLE complications(
-    id INT PRIMARY KEY,
-    complication TEXT NOT NULL
+    id              INT PRIMARY KEY,
+    complication    TEXT NOT NULL
 );
 
 CREATE TABLE velages_complications(
-    velage_id INT NOT NULL,
+    velage_id       INT NOT NULL,
     complication_id INT NOT NULL,
-    FOREIGN KEY(velage_id) REFERENCES velages(id),
-    FOREIGN KEY(complication_id) REFERENCES complications(id),
-    PRIMARY KEY(velage_id, complication_id)
+    FOREIGN KEY (velage_id) REFERENCES velages(id),
+    FOREIGN KEY (complication_id) REFERENCES complications(id),
+    PRIMARY KEY (velage_id, complication_id)
 );
