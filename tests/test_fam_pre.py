c011a5585bf40fdc05bb424481740845243f5b66
Florian <florian.delire.1@gmail.com>
2021-05-02 15:57:30 +0200

    Add tests for function list_family used for 3rd chart

--- tests/test_fam_pre.py
+++ tests/test_fam_pre.py
@@ -22,7 +22,10 @@ class TestListFamily(unittest.TestCase):
     def test_list_family(self):
         res = list_family(self.families_ids, self.samples)
         
-        self.assertEqual(type(res), list, "Function list_family not return a list")
+        self.assertEqual(type(res), list, f"Function list_family not return a list")
+
+        self.assertEqual(len(res), 165, f"Function list_family doesn't return a list of len 165 but of lenght {len(res)}")
+
         for val in res:
             self.assertEqual(type(val), int, f"Function list_family not returning an int value but value of type {type(val)}")
 
