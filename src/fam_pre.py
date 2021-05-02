5be31e093d131826a26d873f8fe99b6930c8295d
Florian <florian.delire.1@gmail.com>
2021-05-02 15:09:13 +0200

    Update function to fit usage

--- src/fam_pre.py
+++ src/fam_pre.py
@@ -1,15 +1,15 @@
 from typing import List
 
 
-def list_deces_prematures_family(families_ids, list_birth) -> List[int]:
-    """crée une liste des morts prématurés pour chaque famille
+def list_family(families_ids, list_fams) -> List[int]:
+    """crée une liste des morts prématurés ou des vivants pour chaque famille
     Args:
-        list_birth (List[tuple]): Une liste de tuple contenant l'id de chaque famille d'animaux mort prématurément
+        list_fams (List[tuple]): Une liste de tuple contenant l'id de chaque famille d'animaux mort prématurément ou des vivants
     Returns:
-        Une liste contenant le nombre de morts prématurés par famille 
+        Une liste contenant le nombre de morts prématurés par famille ou une liste contenant le nombre de vivants par famille
     """
     deaths_by_family = {family_id: 0 for family_id in families_ids}
-    for animal_family in list_birth:
+    for animal_family in list_fams:
         deaths_by_family[animal_family[0]] += 1
 
     return list(deaths_by_family.values())
